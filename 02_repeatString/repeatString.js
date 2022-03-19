b8b1dfb35a6893c008dce8c79dac5c8e7f902f4f
hazardman17 <hazardman@gmail.com>
2022-03-18 18:32:08 -0700

    Update JavaScript and Test Files For Repeat String Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) {return 'ERROR';
+}
+    let myString = '';
+    for(i = 0; i < num; i++) {
+        myString = myString.concat(string);
+    }
+    return myString;
 };
 
 // Do not edit below this line
