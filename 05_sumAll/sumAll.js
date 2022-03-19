46dde92af5faa6a51e562a8fbf626648337d4d3a
hazardman17 <hazardman@gmail.com>
2022-03-18 18:28:37 -0700

    Update JavaScript and Test Files For Sum All Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    let swap = 0;
 
+    // handle NaN
+    if((typeof(firstNum) != 'number') || ((typeof(secondNum) != 'number'))) {
+        return 'ERROR';
+    }
+    
+    // handle first number bigger than second
+    if(secondNum < firstNum) {
+        swap = firstNum;
+        firstNum = secondNum;
+        secondNum = swap;
+    }
+    
+    // handle negative numbers
+    if(firstNum < 0 || secondNum < 0) {
+        return 'ERROR';
+    }
+    
+    for(i=firstNum; i<=secondNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
