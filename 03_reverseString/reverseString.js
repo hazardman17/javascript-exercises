ffa598a3b0a78f542b6c9ae3e6840bdeb35d19c5
hazardman17 <hazardman@gmail.com>
2022-03-18 18:31:11 -0700

    Update JavaScript and Test Files For Reverse String Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    
+    var splitString = string.split(""); 
+ 
+    var reverseArray = splitString.reverse(); 
+    
+    var joinArray = reverseArray.join(""); 
+    
+    return joinArray; 
+}
 // Do not edit below this line
 module.exports = reverseString;
